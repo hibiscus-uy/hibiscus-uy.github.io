6373c3ba5114973d810b1e58b491bf3af897b4ce
Falucho <faller222@gmail.com>
2022-02-17 17:52:42 -0300

    Deploy

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,7 +1,13 @@
 <template>
 <footer>
   <div class="links">
-  Home - Services - About us - Contact us
+    <nuxt-link :class="{'active':$route.hash==='#home'}" :to="{ path: '/', hash:'#home'}">Home</nuxt-link>
+    <span>-</span>
+    <nuxt-link :class="{'active':$route.hash==='#services'}" :to="{ path: '/', hash:'#services'}">Services</nuxt-link>
+    <span>-</span>
+    <nuxt-link :class="{'active':$route.hash==='#about'}" :to="{ path: '/', hash:'#about'}">About us</nuxt-link>
+    <span>-</span>
+    <nuxt-link :class="{'active':$route.hash==='#contact'}" :to="{ path: '/', hash:'#contact'}">Contact us</nuxt-link>
   </div>
   <div class="msg">
     made with <img id="heart" alt="heart" src="@/assets/img/hibiscus_heart.svg"> by us <img id="hibiscus" alt="hibiscus logo" src="@/assets/img/hibiscus_off_white.svg">
@@ -25,7 +31,21 @@ footer{
   justify-content: space-evenly;
 
   .links{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    max-width: 900px;
+    margin-right: auto;
+    margin-left: auto;
+
     font-size: 1rem;
+    width: 100%;
+
+    a{
+
+    color: #FFFFFF;
+    }
   }
   .msg{
     font-size: 0.8rem;
